2ef7d621d4ae2fc5ccad05b1fa78bf1a852235bf
Aliaksandr Puhauka <rayden@sam-solutions.net>
2019-05-31 13:53:49 +0300

    Cleaning tests

--- src/main/java/com/samsolution/demo/validation/ErrorDetails.java
+++ src/main/java/com/samsolution/demo/validation/ErrorDetails.java
@@ -2,11 +2,13 @@ package com.samsolution.demo.validation;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@Setter
 public class ErrorDetails {
     private LocalDateTime timestamp;
     private String exceptionMessage;
--- src/test/java/com/samsolution/demo/controller/EmployeeControllerTest.java
+++ src/test/java/com/samsolution/demo/controller/EmployeeControllerTest.java
@@ -1,12 +1,10 @@
 package com.samsolution.demo.controller;
 
 import com.samsolution.demo.entity.Employee;
-import com.samsolution.demo.jpa.EmployeeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -14,14 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,18 +23,10 @@ public class EmployeeControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @MockBean
-    private EmployeeRepository dao;
 
     @Test
     public void getAllEmployees() {
-        //given
         final int EMPLOYEES_COUNT = 10;
-        List<Employee> employeeList = IntStream.rangeClosed(1, EMPLOYEES_COUNT)
-                .mapToObj(this::createEmployee)
-                .collect(Collectors.toList());
-
-        when(dao.findAll()).thenReturn(employeeList);
 
         //when
         ResponseEntity<List<Employee>> responseList = restTemplate.exchange(
@@ -57,27 +42,17 @@ public class EmployeeControllerTest {
         List<Employee> employees = responseList.getBody();
         assertThat(employees).isNotNull();
         assertThat(employees.size()).isEqualTo(EMPLOYEES_COUNT);
-        assertThat(employees).containsAll(employeeList);
     }
 
-    private Employee createEmployee(int i) {
-        Employee employee = new Employee();
-        employee.setLastName("LastName" + i);
-        employee.setFirstName("FirstName" + i);
-
-        LocalDate randomDate = generateRandomDate();
-        employee.setBirthday(randomDate);
-
-        return employee;
+    @Test
+    public void getEmployeeById() {
+        //when
+        ResponseEntity<String> response = restTemplate.exchange(
+                "/employee/99",
+                HttpMethod.GET,
+                null,
+                String.class);
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
-
-    private LocalDate generateRandomDate() {
-        long minDay = LocalDate.of(1970, 1, 1).toEpochDay();
-        long maxDay = LocalDate.of(2000, 12, 31).toEpochDay();
-        long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
-
-        LocalDate randomDate = LocalDate.ofEpochDay(randomDay);
-        return randomDate;
-    }
-
 }
